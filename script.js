59f663c99581c17f25c594ba2b81ce9f996ec049
Austin L <austinledin28@icloud.com>
2026-02-01 09:52:25 -0600

    began rework by combining all pages into one file, is currently not seperated into different pages all on one for now. Also commented out menu btn and js script for it.

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 
-const menuBtn = document.getElementById('menuBtn');
-const dropdownMenu = document.getElementById('dropdownMenu');
+// const menuBtn = document.getElementById('menuBtn');
+// const dropdownMenu = document.getElementById('dropdownMenu');
 
-menuBtn.addEventListener('mouseenter', function() {
-    dropdownMenu.classList.add('active');
-});
+// menuBtn.addEventListener('mouseenter', function() {
+//     dropdownMenu.classList.add('active');
+// });
 
-menuBtn.addEventListener('mouseleave', function() {
-    dropdownMenu.classList.remove('active');
-});
+// menuBtn.addEventListener('mouseleave', function() {
+//     dropdownMenu.classList.remove('active');
+// });
 
-dropdownMenu.addEventListener('mouseleave', function() {
-    dropdownMenu.classList.remove('active');
-});
+// dropdownMenu.addEventListener('mouseleave', function() {
+//     dropdownMenu.classList.remove('active');
+// });
